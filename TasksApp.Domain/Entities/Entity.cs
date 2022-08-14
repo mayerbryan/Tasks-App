8779ca3e5d8a1db12dff2b35dbfd82fc2c94d8b5
Mayer <Mayer@DESKTOP-JH6VAHS>
2022-08-14 14:21:21 +0100

    initial commit with base itens for the tasks app

---
+++ TasksApp.Domain/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace tasksapp.domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Guid Id { get; private set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public bool Equals(Entity? other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
---
+++ TasksApp.Domain/Entities/TaskItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace tasksapp.domain.Entities
+{
+    public class TaskItem : Entity
+    {
+        
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public bool Done { get; private set; } 
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+
+        public TaskItem(string title, string description, bool done, DateTime date, string user)
+        {
+            Title = title;
+            Description = description;
+            Done = false;
+            Date = date;
+            User = user;
+        }
+
+        public void MarkAsDone() { Done = true; }
+        public void MarkAsUndone() { Done = false; }
+        public void UpdateTitle(string title) { Title = title; }
+        public void UpdateDescription(string description) { Description = description; }
+    }
+}
